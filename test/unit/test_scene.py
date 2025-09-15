481b9235a47e91e6120d8f67ef67bc9603b14a08
Louie Lu <git@louie.lu>
2025-09-15 14:55:43 -0400

    Add scene unit test

---
+++ test/unit/test_scene.py
@@ -0,0 +1,48 @@
+#
+# SPDX-FileCopyrightText: Copyright (c) 2021-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import pytest
+import drjit as dr
+import mitsuba as mi
+import sionna
+from sionna.rt import load_scene, SceneObject, ITURadioMaterial
+
+
+def test_scene_close_and_reuse_radio_material():
+    s1 = sionna.rt.Scene()
+    s2 = sionna.rt.Scene()
+
+    # Add m1 to s1
+    m1 = ITURadioMaterial("m1", "concrete", 1.0)
+    s1.add(m1)
+
+    with pytest.raises(ValueError):
+        # Should not be able to add the same material twice to the same scene
+        s2.add(m1)
+
+    # Should have the ability to "close" the scene
+    s1.close()
+
+    # Should allow to add the radio material to another scene
+    s2.add(m1)
+
+
+def test_scene_remove_radio_material():
+    s1 = sionna.rt.Scene()
+    s2 = sionna.rt.Scene()
+
+    # Add m1 to s1
+    m1 = ITURadioMaterial("m1", "concrete", 1.0)
+    s1.add(m1)
+
+    with pytest.raises(ValueError):
+        # Should not be able to add the same material twice to the same scene
+        s2.add(m1)
+
+    # Remove it from s1
+    s1.remove("m1")
+
+    # Should allow to add the radio material to another scene
+    s2.add(m1)
