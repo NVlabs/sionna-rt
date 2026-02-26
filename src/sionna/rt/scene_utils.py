f56387c2a9d4113bc2aee2574bc1112bd6e8a3cb
hms0411rt <259719759+hms0411rt@users.noreply.github.com>
2026-02-26 18:40:11 +0000

    Update scene_utils.py
    Wrong indent

--- src/sionna/rt/scene_utils.py
+++ src/sionna/rt/scene_utils.py
@@ -65,7 +65,7 @@ def process_xml(xml_string: str,
 
         # Ensure compatibility with Sionna v0.x ITU radio materials
         name = mat_id
-	if name is None:
+		if name is None:
             bsdf_string = ET.tostring(bsdf, encoding='unicode')
             raise ValueError(f"Found bsdf without name: {bsdf_string[:100]}")
         if name.startswith("mat-"):
